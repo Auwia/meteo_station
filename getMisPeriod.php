945e046e339017f3ac9b72d220c22566e599f951
Massimo Manganiello <massimo.manganiello@gmail.com>
2023-11-26 16:10:11 +0100

    Update getMisPeriod.php

--- getMisPeriod.php
+++ getMisPeriod.php
@@ -2,39 +2,39 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
+// Database connection
 $conn = new SQLite3('meteo.db');
-  
+
 $input = $_GET["period"];
 $input2 = $_GET["date"];
 
 $format = 'Y-m-d';
-$d = DateTime::createFromFormat($format, $input);
+$d = DateTime::createFromFormat($format, $input2);
 
 if ($input == "Day") {
-  $query = "select a.location, a.timestamp as timestamp, round(IFNULL(temperature, 0), 2) as temperature, round(IFNULL(humidity, 0), 2) as humidity from temperatures a inner join humidities b on (a.timestamp = b.timestamp) where strftime('%Y-%m-%d', a.timestamp) = date('" . $input2 . "') and strftime('%Y-%m-%d', b.timestamp) = date('" . $input2 . "') order by a.timestamp asc";
+  $query = "SELECT a.location, a.timestamp as timestamp, ROUND(IFNULL(a.temperature, 0), 2) as temperature, ROUND(IFNULL(b.humidity, 0), 2) as humidity FROM temperatures_" . $d->format('Y_m_d') . " a INNER JOIN humidities_" . $d->format('Y_m_d') . " b ON (a.timestamp = b.timestamp) WHERE strftime('%Y-%m-%d', a.timestamp) = date('" . $input2 . "') ORDER BY a.timestamp ASC";
 }
   
 if ($input == "Last 15") {
-  $query = "select * from (SELECT a.timestamp, a.location, IFNULL(round(temperature, 2), 0) AS temperature, IFNULL(round(humidity, 2), 0) AS humidity FROM temperatures a inner join humidities b on (a.timestamp = b.timestamp) order by a.timestamp desc limit 15) order by timestamp";
+  $query = "SELECT * FROM (SELECT a.timestamp, a.location, IFNULL(ROUND(temperature, 2), 0) AS temperature, IFNULL(ROUND(humidity, 2), 0) AS humidity FROM temperatures a INNER JOIN humidities b ON (a.timestamp = b.timestamp) ORDER BY a.timestamp DESC LIMIT 15) ORDER BY timestamp";
 }  
 
 if ($input == "Month") {
-  $query = "select a.location, strftime('%d/%m', a.timestamp) as timestamp, round(AVG(IFNULL(temperature, 0)), 2) as temperature, round(avg(IFNULL(humidity, 0)), 2) as humidity  from temperatures a inner join humidities b on (a.timestamp = b.timestamp) where a.timestamp between date('" . $input2 . "','start of month') and date('" . $input2 . "','start of month','+1 month','-1 day') GROUP BY a.location, strftime('%Y-%m-%d', a.timestamp) order by a.timestamp asc";
+  $query = "SELECT a.location, strftime('%d/%m', a.timestamp) as timestamp, ROUND(AVG(IFNULL(temperature, 0)), 2) as temperature, ROUND(AVG(IFNULL(humidity, 0)), 2) as humidity FROM temperatures_" . $d->format('Y_m') . " a INNER JOIN humidities_" . $d->format('Y_m') . " b ON (a.timestamp = b.timestamp) WHERE a.timestamp BETWEEN date('" . $input2 . "','start of month') AND date('" . $input2 . "','start of month','+1 month','-1 day') GROUP BY a.location, strftime('%Y-%m-%d', a.timestamp) ORDER BY a.timestamp ASC";
 }
   
 if ($input == "6 Months") {
-  $query = "select a.location, strftime('%m/%Y', a.timestamp) as timestamp, round(AVG(IFNULL(temperature, 0)), 2) as temperature, round(avg(IFNULL(humidity, 0)), 2) as humidity  from temperatures a inner join humidities b on (a.timestamp = b.timestamp) where a.timestamp between date('" . $input2 . "','start of month','-6 month') and date('" . $input2 . "','start of month','+1 month','-1 day') GROUP BY a.location, strftime('%Y-%m', a.timestamp) order by a.timestamp asc";
+  $query = "SELECT a.location, strftime('%m/%Y', a.timestamp) as timestamp, ROUND(AVG(IFNULL(temperature, 0)), 2) as temperature, ROUND(AVG(IFNULL(humidity, 0)), 2) as humidity FROM temperatures_" . $d->format('Y_m') . " a INNER JOIN humidities_" . $d->format('Y_m') . " b ON (a.timestamp = b.timestamp) WHERE a.timestamp BETWEEN date('" . $input2 . "','start of month','-6 month') AND date('" . $input2 . "','start of month','+1 month','-1 day') GROUP BY a.location, strftime('%Y-%m', a.timestamp) ORDER BY a.timestamp ASC";
 }
   
 if ($input == "Year") {
-  $query = "select a.location, strftime('%Y/%m', a.timestamp) as timestamp, round(AVG(IFNULL(temperature, 0)), 2) as temperature, round(avg(IFNULL(humidity, 0)), 2) as humidity  from temperatures a inner join humidities b on (a.timestamp = b.timestamp) where a.timestamp between date('" . $input2 . "','start of year') and date('" . $input2 . "','start of month','+1 month','-1 day') GROUP BY a.location, strftime('%Y-%m', a.timestamp) order by a.timestamp asc";
+  $query = "SELECT a.location, strftime('%Y/%m', a.timestamp) as timestamp, ROUND(AVG(IFNULL(temperature, 0)), 2) as temperature, ROUND(AVG(IFNULL(humidity, 0)), 2) as humidity FROM temperatures_" . $d->format('Y') . " a INNER JOIN humidities_" . $d->format('Y') . " b ON (a.timestamp = b.timestamp) WHERE a.timestamp BETWEEN date('" . $input2 . "','start of year') AND date('" . $input2 . "','start of month','+1 month','-1 day') GROUP BY a.location, strftime('%Y-%m', a.timestamp) ORDER BY a.timestamp ASC";
 }
-  
-// echo "<script>console.log('Debug Objects: " . $query . "' );</script>";
+
 $result = $conn->query($query);
 
 $outp = "";
-//while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+
 while ($rs = $result->fetchArray()) {
     if ($outp != "") {$outp .= ",";}
     $outp .= '{"timestamp":"'  . $rs["timestamp"] . '",';
@@ -42,6 +42,7 @@ while ($rs = $result->fetchArray()) {
     $outp .= '"temperature":'. $rs["temperature"]     . ','; 
     $outp .= '"humidity":'. $rs["humidity"]     . '}'; 
 }
+
 $outp ='['.$outp.']';
 $conn->close();
 

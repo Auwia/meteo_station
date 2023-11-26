0adf7ba2c453b21a86768b7a120260d3dd307444
Massimo Manganiello <massimo.manganiello@gmail.com>
2023-11-26 12:36:37 +0100

    Update migration_db_data_to_partition.py

--- migration_db_data_to_partition.py
+++ migration_db_data_to_partition.py
@@ -12,7 +12,7 @@ def create_partition_table(cursor, date):
         timestamp DATE,
         location VARCHAR(255),
         temperature REAL
-    );
+    )
     '''.format(partition_table_name)
 
     cursor.execute(create_partition_table_sql)
@@ -21,38 +21,27 @@ def create_partition_table(cursor, date):
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-# Get the list of existing partitions
-cursor.execute("PRAGMA database_list;")
-databases = cursor.fetchall()
+try:
+    # Get the list of unique dates from the 'temperatures' table
+    cursor.execute("SELECT DISTINCT date(timestamp) FROM temperatures;")
+    unique_dates = [row[0] for row in cursor.fetchall() if row[0] is not None]
 
-# Iterate through each database to get the partition names
-partition_names = []
-for db in databases:
-    db_name = db[1]
-    alias_name = 'temp_' + db_name
-    cursor.execute("ATTACH DATABASE ? AS ?;", (db_name, alias_name))
-    cursor.execute("SELECT name FROM {}.sqlite_master WHERE type='table' AND name LIKE 'temperatures_%';".format(alias_name))
-    partitions = cursor.fetchall()
-    for partition in partitions:
-        partition_names.append(partition[0])
-
-# Get the list of existing dates in the partitions
-existing_dates = [name.split("_")[1] for name in partition_names]
-
-# Get the list of unique dates from the 'temperatures' table
-cursor.execute("SELECT DISTINCT date(timestamp) FROM temperatures;")
-unique_dates = [row[0] for row in cursor.fetchall() if row[0] is not None]
-
-# Create partition tables for dates that do not have partitions yet
-for date in unique_dates:
-    if date not in existing_dates:
+    # Create partition tables for dates that do not have partitions yet
+    for date in unique_dates:
         create_partition_table(cursor, date)
 
-# Move data from the 'temperatures' table to their respective partitions
-for date in existing_dates:
-    cursor.execute("INSERT INTO temperatures_{} SELECT * FROM temperatures WHERE date(timestamp) = ?;".format(date.replace('-', '_')), (date,))
-    cursor.execute("DELETE FROM temperatures WHERE date(timestamp) = ?;", (date,))
+    # Move data from the 'temperatures' table to their respective partitions
+    for date in unique_dates:
+        cursor.execute("INSERT INTO temperatures_{} SELECT * FROM temperatures WHERE date(timestamp) = ?;".format(date.replace('-', '_')), (date,))
+        print("Moved data for {} to partition temperatures_{}".format(date, date.replace('-', '_')))
+        print("Number of rows moved:", cursor.rowcount)
 
-# Commit the changes and close the database connection
-conn.commit()
-conn.close()
+    # Commit the changes and close the database connection
+    conn.commit()
+    conn.close()
+
+except sqlite3.Error as e:
+    print("SQLite error:", e)
+
+except Exception as ex:
+    print("Error:", ex)

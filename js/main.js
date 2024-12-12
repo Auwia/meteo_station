0b33173fa68fbc7292f6b0c73beb9ca195b88790
Auwia <massimo.manganiello@gmail.com>
2024-12-12 10:07:13 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -1,176 +1,155 @@
 var grovyApp = angular.module('grovyApp', ['AngularChart']);
 
-grovyApp.controller("GrovyCtrl", function($scope, $http){
-
+grovyApp.controller("GrovyCtrl", function ($scope, $http) {
+  // Opzioni di periodo disponibili
   $scope.period = ["Last 35", "Day", "Month", "6 Months", "Year"];
-  
-  var period_default = "Day";
+
+  // Inizializzazione dei valori di default
   $scope.selectedPeriod = "Day";
-  $scope.day = moment().format('YYYY-MM-DD');
-  $scope.to = moment().format('YYYY-MM-DD');
-  $scope.from = moment().format('YYYY-MM-DD');
-  
+  $scope.day = new Date(); // Usa un oggetto Date per il campo "date"
+  $scope.lineChartXDataOut = [];
+  $scope.lineChartYDataOut = [];
+  $scope.lineChartXDataPress = [];
+  $scope.lineChartYDataPress = [];
+
+  // Carica i dati iniziali
   function loadData() {
+    console.log("Caricamento dati per il periodo:", $scope.selectedPeriod, "e la data:", $scope.day);
     getMis($scope, $http, $scope.selectedPeriod);
-    // getMisPressure($scope, $http, $scope.selectedPeriod);
+//    getMisPressure($scope, $http, $scope.selectedPeriod);
   }
 
+  // Funzione per aggiornamento manuale
   $scope.manualUpdate = function () {
-    console.log('Button clicked (via ng-click)');
+    console.log("Aggiornamento manuale avviato");
     loadData();
   };
 
-  function getMis($scope, $http, period){
-    var rest_response = {};
-    // Outside
-    var timestamps_out = []; 
-    var areas_out = [];
-    var temperatures_out = [];
-    var humidity_out = [];
-    
-    $http.get('php/getMisPeriod.php/?period=' + $scope.selectedPeriod + '&date=' + moment($scope.day).format('YYYY-MM-DD'))
-	.then(function(response){
-      		rest_response = response.data;
-      		for(var i=0; i < rest_response.length; i++){
-        
-        		// outside
-       		 	if (rest_response[i]['location'] == 'outside') {
-       			   	var tmp = rest_response[i]['timestamp'];
+  // Funzione per ottenere dati meteo generali
+  function getMis($scope, $http, period) {
+    var selectedDate = moment($scope.day).format("YYYY-MM-DD");
+    console.log("Richiesta dati meteo generali per il periodo:", period, "e la data:", selectedDate);
 
-          		if ($scope.selectedPeriod == 'Last 35'){
-            			var lastFive = tmp.substring(11, tmp.length-3);
-          		}
-          
-          		if ($scope.selectedPeriod == 'Day') {
-            			var lastFive = tmp.substr(tmp.length - 8, 5);
-          		}  
+    $http
+      .get("php/getMisPeriod.php/?period=" + period + "&date=" + selectedDate)
+      .then(function (response) {
+        if (response.data && Array.isArray(response.data)) {
+          var rest_response = response.data;
 
-          		if ($scope.selectedPeriod == 'Month'){
-            			var lastFive = tmp;
-          		} 
-          
-          		if ($scope.selectedPeriod == 'Year'){
-            			var lastFive = tmp.substring(0,7);
-          		}
-          
-          		if ($scope.selectedPeriod == '6 Months'){
-            			var lastFive = tmp.substring(0,7);
-          		}    
+          var timestamps_out = [];
+          var temperatures_out = [];
+          var humidity_out = [];
 
-          		timestamps_out.push(lastFive);
-          		areas_out.push(rest_response[i]['location']);
-          		temperatures_out.push(rest_response[i]['temperature']);
-          		humidity_out.push(rest_response[i]['humidity']);
-        	}
-      	}
-      
-      var data_out = {
-        "xDataOut": timestamps_out,
-        "yDataOut":[{
-          "name": "Temperature [°C]",
-          "data": temperatures_out
-        },{
-          "name": "Humidity [%]",
-          "data": humidity_out
-        }]
-      };
-      
-      $scope.lineChartXDataOut=data_out.xDataOut;
-      $scope.lineChartYDataOut=data_out.yDataOut;
-    });
+          rest_response.forEach(function (item) {
+            if (item.location === "outside") {
+              var tmp = item.timestamp;
+              var lastFive;
+
+              if (period === "Last 35") lastFive = tmp.substring(11, tmp.length - 3);
+              if (period === "Day") lastFive = tmp.substr(tmp.length - 8, 5);
+              if (period === "Month") lastFive = tmp;
+              if (period === "Year") lastFive = tmp.substring(0, 7);
+              if (period === "6 Months") lastFive = tmp.substring(0, 7);
+
+              timestamps_out.push(lastFive);
+              temperatures_out.push(item.temperature);
+              humidity_out.push(item.humidity);
+            }
+          });
+
+          $scope.lineChartXDataOut = timestamps_out;
+          $scope.lineChartYDataOut = [
+            { name: "Temperature [°C]", data: temperatures_out },
+            { name: "Humidity [%]", data: humidity_out },
+          ];
+
+          console.log("Dati meteo generali caricati:", $scope.lineChartYDataOut);
+        } else {
+          console.error("Formato dati non valido:", response.data);
+        }
+      })
+      .catch(function (error) {
+        console.error("Errore durante il caricamento dei dati generali:", error);
+      });
   }
 
-function getMisPressure($scope, $http, period){
-  var rest_response_press = {};
-  // Pressure
-  var timestamps_press = [];
-  var areas_press = [];
-  var pressures = [];
-  var temperatures_press = [];
-  var altitudes = [];
+  // Funzione per ottenere dati meteo della pressione
+  function getMisPressure($scope, $http, period) {
+    var selectedDate = moment($scope.day).format("YYYY-MM-DD");
+    console.log("Richiesta dati pressione per il periodo:", period, "e la data:", selectedDate);
 
-  $http.get('php/getMisPressure.php/?period=' + period + '&date=' + moment($scope.day).format('YYYY-MM-DD')).then(function(response){
-    rest_response_press = response.data;
-    for(var i=0; i < rest_response_press.length; i++){
+    $http
+      .get("php/getMisPressure.php/?period=" + period + "&date=" + selectedDate)
+      .then(function (response) {
+        if (response.data && Array.isArray(response.data)) {
+          var rest_response_press = response.data;
 
-      // pressure
-      if (rest_response_press[i]['location'] == 'outside') {
-        var tmp = rest_response_press[i]['timestamp'];
+          var timestamps_press = [];
+          var pressures = [];
+          var temperatures_press = [];
+          var altitudes = [];
 
-        if (period == 'Last 35'){
-          var lastFive = tmp.substring(11, tmp.length-3);
+          rest_response_press.forEach(function (item) {
+            if (item.location === "outside") {
+              var tmp = item.timestamp;
+              var lastFive;
+
+              if (period === "Last 35") lastFive = tmp.substring(11, tmp.length - 3);
+              if (period === "Day") lastFive = tmp.substr(tmp.length - 8, 5);
+              if (period === "Month") lastFive = tmp;
+              if (period === "Year") lastFive = tmp.substring(0, 7);
+              if (period === "6 Months") lastFive = tmp.substring(0, 7);
+
+              timestamps_press.push(lastFive);
+              pressures.push(item.pressure / 1000); // Conversione in kPa
+              temperatures_press.push(item.temperature);
+              altitudes.push(item.altitude);
+            }
+          });
+
+          $scope.lineChartXDataPress = timestamps_press;
+          $scope.lineChartYDataPress = [
+            { name: "Pressure [hPa]", data: pressures },
+            { name: "Temperature [°C]", data: temperatures_press },
+            { name: "Altitude [m]", data: altitudes },
+          ];
+
+          console.log("Dati pressione caricati:", $scope.lineChartYDataPress);
+        } else {
+          console.error("Formato dati non valido per la pressione:", response.data);
         }
+      })
+      .catch(function (error) {
+        console.error("Errore durante il caricamento dei dati di pressione:", error);
+      });
+  }
 
-        if (period == 'Day') {
-          var lastFive = tmp.substr(tmp.length - 8, 5);
-        }
-
-        if (period == 'Month'){
-          var lastFive = tmp;
-        }
-
-        if (period == 'Year'){
-          var lastFive = tmp.substring(0,7);
-        }
-
-        if (period == '6 Months'){
-          var lastFive = tmp.substring(0,7);
-        }
-
-        timestamps_press.push(lastFive);
-        areas_press.push(rest_response_press[i]['location']);
-        pressures.push(rest_response_press[i]['pressure'] / 1000);
-        temperatures_press.push(rest_response_press[i]['temperature']);
-        altitudes.push(rest_response_press[i]['altitude']);
-      }
-    }
-
-    var data_press = {
-      "xDataPress": timestamps_press,
-      "yDataPress":[{
-        "name": "Pressure [hPa]",
-        "data": pressures
-      },{
-        "name": "Temperature [°C]",
-        "data": temperatures_press
-      },{
-        "name": "Altitude [m]",
-        "data": altitudes
-      }]
-    };
-
-    $scope.lineChartXDataPress=data_press.xDataPress;
-    $scope.lineChartYDataPress=data_press.yDataPress;
-
-console.log($scope.lineChartYDataPress);
-
-  });
-}
-
+  // Funzione per validare le date
   function isValid(value) {
-    // STRING FORMAT yyyy-mm-dd
-    if(value=="" || value==null){return false;}                
-    
-    // m[1] is year 'YYYY' * m[2] is month 'MM' * m[3] is day 'DD'          
+    if (!value) return false;
     var m = value.match(/(\d{4})-(\d{2})-(\d{2})/);
-    
-    // STR IS NOT FIT m IS NOT OBJECT
-    if( m === null || typeof m !== 'object'){return false;}        
-    
-    // CHECK m TYPE
-    if (typeof m !== 'object' && m !== null && m.size!==3){return false;}
-          
-    var ret = true; //RETURN VALUE            
-    var thisYear = new Date().getFullYear(); //YEAR NOW
-    var minYear = 1999; //MIN YEAR
-    
-    // YEAR CHECK
-    if( (m[1].length < 4) || m[1] < minYear || m[1] > thisYear){ret = false;}
-    // MONTH CHECK      
-    if( (m[2].length < 2) || m[2] < 1 || m[2] > 12){ret = false;}
-    // DAY CHECK
-    if( (m[3].length < 2) || m[3] < 1 || m[3] > 31){ret = false;}
-    
-    return ret;    
+    if (!m || m.length !== 4) return false;
+
+    var year = parseInt(m[1], 10);
+    var month = parseInt(m[2], 10);
+    var day = parseInt(m[3], 10);
+
+    var today = new Date();
+    if (year < 1999 || year > today.getFullYear()) return false;
+    if (month < 1 || month > 12) return false;
+    if (day < 1 || day > 31) return false;
+
+    return true;
   }
+
+  // Aggiorna i dati quando cambia il periodo selezionato
+  $scope.$watch("selectedPeriod", function (newVal, oldVal) {
+    if (newVal !== oldVal) {
+      console.log("Periodo selezionato cambiato:", newVal);
+      loadData();
+    }
+  });
+
+  // Carica i dati iniziali
+  loadData();
 });
